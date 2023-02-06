f93d25785720958877996a2f116f8b3a58c6c634
Ryan Costa <ryan.costa122@gmail.com>
2023-02-06 11:40:07 -0300

    first commit

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+import "tailwindcss/tailwind.css";
+import { AppProvider } from "../data/context/AppContext";
+import { AuthProvider } from "../data/context/AuthContext";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <AuthProvider>
+      <AppProvider>
+        <Component {...pageProps} />
+      </AppProvider>
+    </AuthProvider>
+  );
+}
